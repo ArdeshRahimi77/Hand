8b3b8735dfc3222481183173ccd30af38ac91b18
ArdeshRahimi77 <ardesh.imi@gmail.com>
2024-12-22 11:43:48 -0800

    Create booleanhw.java
    First Commit

---
+++ booleanhw.java
@@ -0,0 +1,14 @@
+public class booleanhw {
+    public static void main(String[] args){
+        boolean RightHand = true;
+        boolean LeftHand = false;
+String question = "What hand is the gift in?";
+System.out.println(question);
+if (RightHand){
+    System.out.println(RightHand);
+} else{
+        System.out.println(LeftHand);
+    }
+         }    
+}
+
